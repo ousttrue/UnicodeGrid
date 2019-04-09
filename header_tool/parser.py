2cf4588a593047d0700629a60cb0c0bd2f7a65fc
ousttrue <ousttrue@gmail.com>
2019-04-09 19:25:49 +0900

    WIP dlang

--- header_tool/parser.py
+++ header_tool/parser.py
@@ -1,11 +1,18 @@
+'''
+require
+> pip insall clang
+'''
+
 import pathlib
-from typing import List, Optional
+import os
+from typing import List, Optional, Dict
 from clang import cindex
 
 
 WINDOWS_KITS_INCLUDE_DIR = pathlib.Path(
     'C:/Program Files (x86)/Windows Kits/10/Include')
 
+DEFAULT_DLL = pathlib.Path('C:/Program Files (x86)/LLVM/bin/libclang.dll')
 
 HERE = pathlib.Path(__file__).absolute().parent
 
@@ -41,7 +48,7 @@ class ClangMethod:
                     x.spelling, x.type.spelling))
 
     def __str__(self) -> str:
-        return f'{self.result} {self.name}({", ".join(str(m) for m in self.args)})'
+        return f'{self.result} {self.name}({", ".join(str(m) for m in self.args)});\n'
 
 
 class ClangInterface:
@@ -86,86 +93,118 @@ class ClangInterface:
         self.methods = methods
 
     def __str__(self) -> str:
-        cr = '\n'
-        return f'''{self.uuid}
-{self.name}: {self.base} {{
-{cr.join('    ' + str(m) for m in self.methods)}
-}} '''
+        '''
+        for dlang
+        '''
+        return f'''
+interface {self.name}: {self.base}        
+{{
+    static immutable uuidof = UUID("{self.uuid}");
+{"".join("    " + str(m) for m in self.methods)}}}
+'''
 
 
-class ClangParser:
-    '''
-    require
-    > pip insall clang
-    '''
-
-    def __init__(self, dll: pathlib.Path = None) -> None:
-        self.dll = dll if dll else pathlib.Path(
-            'C:/Program Files (x86)/LLVM/bin/libclang.dll')
+class ClangHeader:
+    def __init__(self) -> None:
         self.interface_list: List[ClangInterface] = []
         self.function_list: List[ClangMethod] = []
-
-    def parse(self, header: pathlib.Path) -> None:
-        cindex.Config.set_library_file(str(self.dll))
-        index = cindex.Index.create()
-        translation_unit = index.parse(str(header), ['-x', 'c++'])
-
-        for child in translation_unit.cursor.get_children():
-            self.traverse(child)
-
-    def print_cursor(self, cursor: cindex.Cursor, level: int) -> None:
-        print(f'{"  " * (level)}{cursor.kind}: {cursor.displayname}')
-
-    def traverse(self, cursor: cindex.Cursor, level=0) -> None:
-        file = pathlib.Path(cursor.location.file.name).name
-        if file not in include_headers:
-            return
-
-        if cursor.kind == cindex.CursorKind.UNEXPOSED_DECL:
-            #print(f'{cursor.kind}: {cursor.spelling}')
-            for child in cursor.get_children():
-                self.traverse(child, level+1)
-
-        elif cursor.kind == cindex.CursorKind.STRUCT_DECL:
-            i = ClangInterface.create(cursor)
-            if i:
-                self.interface_list.append(i)
-
-        elif cursor.kind == cindex.CursorKind.FUNCTION_DECL:
-            self.function_list.append(ClangMethod(cursor))
-
-        elif cursor.kind == cindex.CursorKind.VAR_DECL:
-            pass
-
-        else:
-            self.print_cursor(cursor, level)
 
 
 class Kit:
     def __init__(self, kit: pathlib.Path) -> None:
         self.kit = kit
-        self.parser = ClangParser()
 
-    def parse(self) -> None:
-        header = self.kit/'um/d3d11.h'
-        self.parser.parse(header)
+    def parse(self, dll=DEFAULT_DLL) -> Dict[str, ClangHeader]:
+        path = self.kit/'um/d3d11.h'
 
-    def generate(self, dst: pathlib.Path) -> None:
+        cindex.Config.set_library_file(str(dll))
+        index = cindex.Index.create()
+        translation_unit = index.parse(str(path), ['-x', 'c++'])
+
+        headers: Dict[str, ClangHeader] = {}
+
+        def get_or_create_header(location: pathlib.Path) -> ClangHeader:
+            header = headers.get(location.name)
+            if not header:
+                header = ClangHeader()
+                headers[location.name] = header
+            return header
+
+        def print_cursor(cursor: cindex.Cursor, level: int) -> None:
+            print(f'{"  " * (level)}{cursor.kind}: {cursor.displayname}')
+
+        def traverse(cursor: cindex.Cursor, level=0) -> None:
+            file = pathlib.Path(cursor.location.file.name).name
+            if file not in include_headers:
+                return
+
+            if cursor.kind == cindex.CursorKind.UNEXPOSED_DECL:
+                #print(f'{cursor.kind}: {cursor.spelling}')
+                for child in cursor.get_children():
+                    traverse(child, level+1)
+
+            elif cursor.kind == cindex.CursorKind.STRUCT_DECL:
+                i = ClangInterface.create(cursor)
+                if i:
+                    header = get_or_create_header(
+                        pathlib.Path(cursor.location.file.name))
+                    header.interface_list.append(i)
+
+            elif cursor.kind == cindex.CursorKind.FUNCTION_DECL:
+                header = get_or_create_header(
+                    pathlib.Path(cursor.location.file.name))
+                header.function_list.append(ClangMethod(cursor))
+
+            elif cursor.kind == cindex.CursorKind.VAR_DECL:
+                pass
+
+            else:
+                print_cursor(cursor, level)
+
+        for child in translation_unit.cursor.get_children():
+            traverse(child)
+        return headers
+
+    def generate(self, dst: pathlib.Path, headers: Dict[str, ClangHeader]) -> None:
         root = dst / 'windowskits' / f'build_{self.kit.name.replace(".", "_")}'
         root.mkdir(parents=True, exist_ok=True)
+
+        for k, v in headers.items():
+            dst = root / f'{pathlib.Path(k).stem}.d'
+            print(dst)
+
+            with dst.open('w') as s:
+
+                s.write('''// this is generated
+import core.sys.windows.windef;
+import core.sys.windows.com;
+import std.uuid;
+
+extern(Windows){
+                ''')
+
+                for i in v.interface_list:
+                    s.write(str(i)
+                            .replace('&', '*')
+                            .replace('*const *', '**')
+                            )
+                s.write("\n")
+
+                for f in v.function_list:
+                    s.write(str(f)
+                            .replace('&', '*')
+                            )
+
+                s.write('}')
 
 
 def main() -> None:
     kits = [Kit(x) for x in sorted(WINDOWS_KITS_INCLUDE_DIR.iterdir())]
     kit = kits[-1]
-    kit.parse()
-    kit.generate(HERE.parent/'windowskits/source')
 
-    for i in kit.parser.interface_list:
-        print(i)
+    headers = kit.parse()
 
-    for f in kit.parser.function_list:
-        print(f)
+    kit.generate(HERE.parent/'windowskits/source', headers)
 
 
 if __name__ == '__main__':
