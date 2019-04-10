1c1d07b4493932c660bb450652949d214681f8c8
ousttrue <ousttrue@gmail.com>
2019-04-10 16:07:59 +0900

    ComPtr

--- windowskits_sample/source/app.d
+++ windowskits_sample/source/app.d
@@ -1,19 +1,35 @@
 import std.stdio;
 import windowskits.build_10_0_17763_0.dxgi;
 
+struct ComPtr(T)
+{
+	T ptr;
+	alias ptr this;
+	this(this) // post-blit
+	{
+		if (ptr)
+			ptr.AddRef();
+	}
+
+	~this() // destructor
+	{
+		if (ptr)
+			ptr.Release();
+	}
+
+	bool opCast(V)() const if (is(V == bool))
+	{
+		return ptr ? true : false;
+	}
+}
+
 void main()
 {
-	// Create a DXGIFactory object.
-	IDXGIFactory pFactory;
-	auto hr = CreateDXGIFactory(&pFactory.uuidof, cast(void**)&pFactory);
+	ComPtr!IDXGIFactory pFactory;
+	auto hr = CreateDXGIFactory(&pFactory.uuidof, cast(void**)&pFactory.ptr);
 	if (hr != 0)
 	{
 		writeln("fail");
 		return;
 	}
-
-	scope (exit)
-		pFactory.Release();
-
-	return;
 }
