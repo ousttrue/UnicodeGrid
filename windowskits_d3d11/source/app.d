9f1a7b0034bae3c84045ef7e5d1f8e8aa014155f
ousttrue <ousttrue@gmail.com>
2019-04-10 21:47:28 +0900

    ClearRenderTargetView

--- windowskits_d3d11/source/app.d
+++ windowskits_d3d11/source/app.d
@@ -5,6 +5,28 @@ import core.runtime;
 import core.sys.windows.windows;
 import windowskits.build_10_0_17763_0.d3d11;
 
+struct ComPtr(T)
+{
+	T ptr;
+	alias ptr this; // cannot code completion
+	this(this) // post-blit
+	{
+		if (ptr)
+			ptr.AddRef();
+	}
+
+	~this() // destructor
+	{
+		if (ptr)
+			ptr.Release();
+	}
+
+	bool opCast(V)() const if (is(V == bool))
+	{
+		return ptr ? true : false;
+	}
+}
+
 extern (Windows) LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) nothrow
 {
 	switch (message)
@@ -65,19 +87,35 @@ extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	D3D_FEATURE_LEVEL[] levels = [
 		D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL.D3D_FEATURE_LEVEL_11_0,
 	];
-	ID3D11Device pDevice;
+	ComPtr!ID3D11Device pDevice;
 	D3D_FEATURE_LEVEL level;
-	ID3D11DeviceContext pDeviceContext;
+	ComPtr!ID3D11DeviceContext pDeviceContext;
 
-	auto result = D3D11CreateDevice(null, D3D_DRIVER_TYPE.D3D_DRIVER_TYPE_HARDWARE,
-			null, 0, levels.ptr, cast(uint) levels.length, 7, &pDevice,
-			&level, &pDeviceContext);
+	DXGI_SWAP_CHAIN_DESC scDesc;
+	scDesc.BufferCount = 1;
+	scDesc.BufferDesc.Width = 0;
+	scDesc.BufferDesc.Height = 0;
+	scDesc.BufferDesc.Format = DXGI_FORMAT.DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	scDesc.BufferDesc.RefreshRate.Numerator = 60;
+	scDesc.BufferDesc.RefreshRate.Denominator = 1;
+	scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scDesc.OutputWindow = hWnd;
+	scDesc.SampleDesc.Count = 1;
+	scDesc.SampleDesc.Quality = 0;
+	scDesc.Windowed = TRUE;
+
+	ComPtr!IDXGISwapChain pSwapChain;
+
+	auto result = D3D11CreateDeviceAndSwapChain(null, D3D_DRIVER_TYPE.D3D_DRIVER_TYPE_HARDWARE, null, 0,
+			levels.ptr, cast(uint) levels.length, D3D11_SDK_VERSION, &scDesc,
+			&pSwapChain.ptr, &pDevice.ptr, &level, &pDeviceContext.ptr);
 	if (result != 0)
 	{
 		return 3;
 	}
 
 	// main loop
+	ComPtr!ID3D11RenderTargetView pRenderTargetView;
 	MSG msg;
 	while (true)
 	{
@@ -87,6 +125,32 @@ extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		}
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+
+		// get backbuffer
+		if (pRenderTargetView.ptr is null)
+		{
+			ComPtr!ID3D11Texture2D pBackBuffer;
+			if (pSwapChain.GetBuffer(0, &ID3D11Texture2D.uuidof,
+					cast(void**)&pBackBuffer.ptr) != 0)
+			{
+				return 4;
+			}
+
+			if (pDevice.CreateRenderTargetView(pBackBuffer.ptr, null, &pRenderTargetView.ptr) != 0)
+			{
+				return 5;
+			}
+		}
+
+		auto renderTargets = [pRenderTargetView.ptr];
+		pDeviceContext.OMSetRenderTargets(cast(uint) renderTargets.length, renderTargets.ptr, null);
+
+		float[4] clearColor = [0.0f, 0.0f, 1.0f, 0.0f];
+		pDeviceContext.ClearRenderTargetView(pRenderTargetView.ptr, clearColor);
+
+		pDeviceContext.Flush();
+
+		pSwapChain.Present(0, 0);
 	}
 	return cast(int) msg.wParam;
 }
